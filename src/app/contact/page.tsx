35a9fe3dc8dcf779ca145dd26a55eafefbd2d7b9
Shahfareeha18 <farishah180@gmail.com>
2026-01-28 19:43:10 +0500

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from 'next';
 import PageLayout from '@/components/layout/page-layout';
 import ContactSection from '@/features/contact/components/contact-section';
-import CTASection from '@/features/home/components/cta-section';
+
 
 export const metadata: Metadata = {
   title: 'Contact Us - Syeda Fareeha Portfolio',
